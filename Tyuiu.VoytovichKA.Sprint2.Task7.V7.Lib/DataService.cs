c68318cc2fed9c1be3a1c462797b4e1deb61aef7
VoytovichKlimentiy <Tyuiu.VoytovichKA@outlook.com>
2026-09-13 18:12:12 +0500

    Task7

---
+++ Tyuiu.VoytovichKA.Sprint2.Task7.V7.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System.Timers;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.VoytovichKA.Sprint2.Task7.V7.Lib
+{
+    public class DataService : ISprint2Task7V7
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            if (y < 2 && y > 0)
+            {
+                if (y < (2 - x * x) && x > -Math.Sqrt(2) && x < Math.Sqrt(2))
+                    return true;
+                else return false;
+            }
+            else if (y < 0 && y > -2)
+            {
+                if (y > x && y < (2 - x * x)) return true;
+                else return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Tyuiu.VoytovichKA.Sprint2.Task7.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.VoytovichKA.Sprint2.Task7.V7.Lib;
+
+namespace Tyuiu.VoytovichKA.Sprint2.Task7.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void IsValid()
+        {
+            DataService ds = new DataService();
+            double x = 0.5, y = 0.5;
+            bool res=ds.CheckDotInShadedArea(x, y);
+            Assert.IsTrue(res);
+        }
+    }
+}
